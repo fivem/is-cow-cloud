6498b620f2d6b2e909a5496346fea1a096cc474a
fivem <forever113@126.com>
2021-01-12 22:03:55 +0800

    Initial commit

---
+++ src/main/java/com/fivem/iscow/iscowcloud/IsCowCloudApplication.java
@@ -0,0 +1,13 @@
+package com.fivem.iscow.iscowcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsCowCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IsCowCloudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fivem/iscow/iscowcloud/IsCowCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fivem.iscow.iscowcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsCowCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
